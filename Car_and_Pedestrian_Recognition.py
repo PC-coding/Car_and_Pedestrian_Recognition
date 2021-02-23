b65a743822082cabdcb30ecfc626e306b00ff334
PC-coding <partapchauhan8@gmail.com>
2021-02-23 12:46:23 -0500

    pretrained data added to code + reading car image

--- Car_and_Pedestrian_Recognition.py
+++ Car_and_Pedestrian_Recognition.py
@@ -1,5 +1,14 @@
 import cv2
 
-img = 'car_traffic.jpg'
+#car image
+car_img = 'car_traffic.jpg'
+
+# pre-trained car classifying data
+car_detector = 'cars_haar_cascades.xml'
+
+# creating opencv image
+img = cv2.imread(car_img)
+
+
 
 print('successful')

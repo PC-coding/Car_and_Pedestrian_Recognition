72cc6642bbd85e88befa418fccd3dc093e77dee2
PC-coding <partapchauhan8@gmail.com>
2021-02-24 00:53:41 -0500

    creating car classifier

--- Car_and_Pedestrian_Recognition.py
+++ Car_and_Pedestrian_Recognition.py
@@ -4,10 +4,13 @@ import cv2
 car_img = 'car_traffic.jpg'
 
 # pre-trained car classifying data
-car_detector = 'cars_haar_cascades.xml'
+classifier= 'cars_haar_cascades.xml'
 
 # creating opencv image
 img = cv2.imread(car_img)
+
+# creating car recognition
+car_detector = cv2.CascadeClassifier(classifier)
 
 # displaying image with the cars spotted
 cv2.imshow('Car Recognition', img)

6e388ef5cfac04f3241119ed668fa01124012ff7
PC-coding <partapchauhan8@gmail.com>
2021-03-01 16:49:29 -0500

    displays cars in video and closes program when key is pressed

--- Car_and_Pedestrian_Recognition.py
+++ Car_and_Pedestrian_Recognition.py
@@ -8,7 +8,7 @@ car_video = cv2.VideoCapture('tesla_car_crash.mov')
 classifier= 'cars_haar_cascades.xml'
 
 # creating opencv image
-img = cv2.imread(car_img)
+# img = cv2.imread(car_img)
 
 # code to read car movement in streets via tesla accident model video
 # run forever until vehicles stop
@@ -23,25 +23,30 @@ while True:
     else:
         break
 
+    # displays images with cars spotted
+    cv2.imshow('Car Recognition', grayscale_frame)
+
+    # waits till any key is pressed to close python program
+    cv2.waitKey()
 
 # converting image to grayscale
-grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # creating car recognition
-car_tracker = cv2.CascadeClassifier(classifier)
+# car_tracker = cv2.CascadeClassifier(classifier)
 
 # car detection
-car_detector = car_tracker.detectMultiScale(grayscale) 
-print(car_detector)
+# car_detector = car_tracker.detectMultiScale(grayscale) 
+# print(car_detector)
 
 # drawing boxes around cars that are detected
-for (x, y, w, h) in car_detector:
-    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+# for (x, y, w, h) in car_detector:
+#     cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
 # displaying image with the cars spotted
-cv2.imshow('Car Recognition', img)
+# cv2.imshow('Car Recognition', img)
 
 # Waits till any key is pressed to close python program
-cv2.waitKey()
+# cv2.waitKey()
 
 print('successful')

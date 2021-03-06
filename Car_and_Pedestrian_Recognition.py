666e6f7dd36f258a60d05a4d26c36405d2d7b7b5
PC-coding <partapchauhan8@gmail.com>
2021-03-06 15:39:55 -0500

    cleaning up code

--- Car_and_Pedestrian_Recognition.py
+++ Car_and_Pedestrian_Recognition.py
@@ -9,10 +9,13 @@ car_video = cv2.VideoCapture('tesla_car_crash.mov')
 
 # pre-trained car classifying data
 classifier= 'cars_haar_cascades.xml'
+# pre-trained data for pedestrian recognition
+pedestrian_data = 'pedestrian_haar_cascade.xml'
 
 # create car classifier
 car_tracker = cv2.CascadeClassifier(classifier)
-
+# creating pedestrian classifier
+pedestrian_track = cv2.CascadeClassifier('pedestrian_haar_cascade.xml')
 
 # code to read car movement in streets via tesla accident model video
 # run forever until vehicles stop
@@ -29,14 +32,18 @@ while True:
 
     # detecting cars in video
     car_detector = car_tracker.detectMultiScale(grayscale_frame)
-    print(cars)
+    # detect pedestrians
+    pedestrians = pedestrian_track.detectMultiScale(grayscale_frame)
+    print(pedestrians)
 
-    # drawing boxes around cars that are detected
+    # drawing boxes around cars and pedestrians that are detected
     for (x, y, w, h) in car_detector:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+    for (x, y, w, h) in pedestrians:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 255), 2)
 
     # displays images with cars spotted
-    cv2.imshow('Car Recognition', frame)
+    cv2.imshow('Car and Pedestrian Recognition', frame)
 
     # waits till any key is pressed to close python program
     cv2.waitKey(1)
@@ -51,44 +58,3 @@ while True:
 # cv2.imshow('Car Recognition', img)
 
 print('successful')
-
-
-## Pedstrian Recognition
-
-# pre-trained data for pedestrian recognition
-pedestrian_data = 'pedestrian_haar_cascade.xml'
-
-# creating pedestrian classifier
-pedestrian_track = cv2.CascadeClassifier('pedestrian_haar_cascade.xml')
-
-# run forever until pedestrian doesn't show up
-while True:
-
-    # read the current frame
-    (read_successful, frame) = video.read()
-
-    # conversion to grayscale
-    if read_successful:
-        grayscale_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    else:
-        break
-
-    # detect pedestrians
-    pedestrians = pedestrian_track.detectMultiScale(grayscale_frame)
-
-    # draw rectangular boxes around pedestrians
-    for (x, y, w, h) in pedestrians:
-        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 255), 2)
-
-    # displays image with pedestrians spotted
-    cv2.imshow('Pedestrian Recognition', frame)
-
-    # listens for 1ms to check if any key was pressed, then move on
-    key = cv2.waitKey(1)
-
-    # stop is Q key is pressed
-    if key == 81 or key == 113:
-        break
-    
-    # release video capture
-    video.release()
